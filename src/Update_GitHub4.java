f287a80dcf95bf5568de25a58e5cb9630a7d5fe6
Mates <mates2041@gmail.com>
2023-12-09 22:07:39 +0300

    Fourth commit

---
+++ src/Update_GitHub4.java
@@ -0,0 +1,7 @@
+public class Update_GitHub4 {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba yeni versiyon");
+        System.out.println("Hosgeldin yeni versiyon");
+    }
+}
